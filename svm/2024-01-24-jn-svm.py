0fffa0404dee148faecb5cc7fd7d6ae0b13be6a5
System Administrator <root@Jayatis-MBP.attlocal.net>
2024-01-25 22:20:44 -0800

    SVM soft margin code added

---
+++ svm/2024-01-24-jn-svm.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+get_ipython().system('{sys.executable} -m pip install --user numpy')
+
+
+# In[3]:
+
+
+# Add libraries
+import numpy as np
+
+
+# In[1]:
+
+
+# Take input 
+x = np.random.rand(3,2)
+print(x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
